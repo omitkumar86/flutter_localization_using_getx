583181115c6b7b4dae92b0547aa51dedebcf65ca
omit-excelitai <omit2023@excelitai.com>
2023-06-12 17:42:18 +0600

    Updated

--- lib/controller/locale_controller.dart
+++ lib/controller/locale_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
 
 class LocaleController extends GetxController {
   Locale? _locale;
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,67 +11,71 @@ class HomePage extends StatelessWidget {
   static const String routeName = '/home_page';
   HomePage({super.key});
 
-  final localeController = Get.find<LocaleController>();
+  //final localeController = Get.find<LocaleController>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: AppColor.appBarColor,
-        title: Text(AppLocalizations.of(context)!.title.toString()),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              AppLocalizations.of(context)!.greeting.toString(),
-              style: TextStyle(
-                  fontSize: 25.sp,
-                  fontWeight: FontWeight.w600,
-                  color: AppColor.primaryOrange),
+    return GetBuilder<LocaleController>(
+        init: LocaleController(),
+        builder: (localeController) {
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: AppColor.appBarColor,
+              title: Text(AppLocalizations.of(context)!.title.toString()),
             ),
-            SizedBox(height: 10.h),
-            Text(
-              AppLocalizations.of(context)!.address.toString(),
-              style: TextStyle(fontSize: 23.sp),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!.greeting.toString(),
+                    style: TextStyle(
+                        fontSize: 25.sp,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.primaryOrange),
+                  ),
+                  SizedBox(height: 10.h),
+                  Text(
+                    AppLocalizations.of(context)!.address.toString(),
+                    style: TextStyle(fontSize: 23.sp),
+                  ),
+                  SizedBox(height: 30.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        AppLocalizations.of(context)!.switchButton.toString(),
+                        style: TextStyle(
+                            fontSize: 22.sp,
+                            color: AppColor.greenColor,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      FlutterSwitch(
+                        activeText: "EN",
+                        inactiveText: "BN",
+                        activeTextColor: AppColor.primaryWhite,
+                        inactiveTextColor: AppColor.primaryWhite,
+                        activeColor: AppColor.primaryOrange,
+                        inactiveColor: AppColor.primaryOrange,
+                        valueFontSize: 14.0.sp,
+                        toggleSize: 25.0.sp,
+                        value: localeController.locale?.languageCode == 'bn',
+                        borderRadius: 40.0.r,
+                        padding: 5,
+                        showOnOff: true,
+                        onToggle: (value) {
+                          final newLocale =
+                              value ? const Locale('bn') : const Locale('en');
+                          localeController.setLanguage(newLocale.toString());
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 30.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  AppLocalizations.of(context)!.switchButton.toString(),
-                  style: TextStyle(
-                      fontSize: 22.sp,
-                      color: AppColor.greenColor,
-                      fontWeight: FontWeight.w500),
-                ),
-                FlutterSwitch(
-                  activeText: "EN",
-                  inactiveText: "BN",
-                  activeTextColor: AppColor.primaryWhite,
-                  inactiveTextColor: AppColor.primaryWhite,
-                  activeColor: AppColor.primaryOrange,
-                  inactiveColor: AppColor.primaryOrange,
-                  valueFontSize: 14.0.sp,
-                  toggleSize: 25.0.sp,
-                  value: localeController.locale?.languageCode == 'bn',
-                  borderRadius: 40.0.r,
-                  padding: 5,
-                  showOnOff: true,
-                  onToggle: (value) {
-                    final newLocale =
-                        value ? const Locale('bn') : const Locale('en');
-                    localeController.setLanguage(newLocale.toString());
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
